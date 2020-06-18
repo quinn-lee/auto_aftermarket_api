78309f6a677ac19401ffcb73e1b6c905d7ab0a57
lifuyuan <lifuyuan33@gmail.com>
2020-06-18 10:55:20 +0800

    n

--- models/sub_order.rb
+++ models/sub_order.rb
@@ -7,8 +7,14 @@ class SubOrder < ActiveRecord::Base
     oss.each do |os|
       items << TSku.find(os.t_sku_id).to_api_order.merge(os.to_api_order)
     end
+    shop_info = {}
+    if order.shop_id.present?
+      shop_info = Shop.find(order.shop_id).to_api_simple
+    end
     {
       sub_type: sub_type,
+      shop_info: shop_info,
+      delivery_info: order.delivery_info,
       items: items
     }
   end
