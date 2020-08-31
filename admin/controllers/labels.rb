91e8f9b4b4716fe522c506aec05022e844636caa
KaterKee <472282495@qq.com>
2020-08-31 15:09:04 +0800

    ..

--- admin/controllers/labels.rb
+++ admin/controllers/labels.rb
@@ -6,6 +6,7 @@ AutoAftermarketApi::Admin.controllers :labels do
   # 优选标签列表
   get :preferred_labels do
     @labels = Label.all.where(ltype: 1)
+    @labels_event = Label.all.where("ltype > 1")
     render "labels/preferred_labels"
   end
 
@@ -29,6 +30,22 @@ AutoAftermarketApi::Admin.controllers :labels do
 
   # 修改标签名称
   post :update_preferred_label, :with => :label_id do
+    begin
+      @label = Label.find(params[:label_id])
+      if @label.update!(params[:label])
+        flash[:success] = "标签修改成功"
+        redirect(url(:labels, :preferred_labels))
+      else
+        raise "标签修改失败"
+      end
+    rescue => e
+      logger.info e.backtrace
+      flash[:error] = e.message
+      redirect(url(:labels, :preferred_labels))
+    end
+  end
+
+  post :update_event_label do
     begin
       @label = Label.find(params[:label_id])
       if @label.update!(params[:label])
