50444c7993af5f2b6a1a2b350224dc5e2689852b
lifuyuan <lifuyuan33@gmail.com>
2020-10-10 15:44:36 +0800

    n

--- api/controllers/v1.0/coupons.rb
+++ api/controllers/v1.0/coupons.rb
@@ -34,7 +34,7 @@ AutoAftermarketApi::Api.controllers :'v1.0', :map => 'v1.0/coupons' do
     api_rescue do
       authenticate
 
-      { status: 'succ', data: @merchant.coupons.map(&:to_api)}.to_json
+      { status: 'succ', data: @merchant.coupons.where.not(title: "分享有礼优惠券").map(&:to_api)}.to_json
     end
   end
 
@@ -65,6 +65,36 @@ AutoAftermarketApi::Api.controllers :'v1.0', :map => 'v1.0/coupons' do
       authenticate
       @coupon = @merchant.coupons.find(@request_params['id'])
       { status: 'succ', data: @coupon.to_api }.to_json
+    end
+  end
+
+  # 分享有礼优惠券详情
+  # params 空
+  # data
+=begin
+    {
+      "id": 1,
+      "title": "分享有礼优惠券",   #优惠券标题
+      "remarks": "满1000元可用",   #优惠券说明
+      "ctype": "满减券", # 满减券或折扣券
+      "end_time": "2020-06-25 21:00:00",  #优惠券结束时间
+      "money": "100.0",   #优惠券金额，满减券时，该字段有值
+      "full_money": "2000.0",   #优惠券使用条件，订单金额满多少可使用，每个订单只可使用一张优惠券，满减券时，该字段有值
+      "discount": null,  # 折扣券时，该字段有值，0-10之间
+      "max_num": 10, # 可领取数量
+      "received_num": 0,  # 已领取数量
+      "spus": [  # 绑定spu 的id列表
+          "1"
+      ],
+      "status": "下架",  #状态： 可领取，下架，已结束； 只有状态是可领取的，客户才能领取
+      "img_path": "public/uploads/coupon/1.png" #图片位置
+    }
+=end
+  post :share_gift, :provides => [:json] do
+    api_rescue do
+      authenticate
+      @coupon = @merchant.coupons.find_by(title: "分享有礼优惠券")
+      { status: 'succ', data: @coupon.try{|c| c.to_api} }.to_json
     end
   end
 
